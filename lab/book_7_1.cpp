a69fbceb50693ee8d4206db9357da45ba128be74
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-13 14:49:28 -0700

    got the toUpper function to work

--- lab/book_7_1.cpp
+++ lab/book_7_1.cpp
@@ -6,11 +6,14 @@ Define a class called Month that is an abstract data type for a month. Your clas
 
 using namespace std;
 
+
+string	toUpper(string str_org);
+
 class	month
 {
 	public:
 	// 1 - January 2 - February .... 12 - Dec
-	int	month_index;
+	int	index;
 
 	month(string month);
 
@@ -19,14 +22,33 @@ class	month
 
 int	main(int argc, char** arv)
 {
-	cout << "Hello world" << endl;
+	string testing = "this is a lower case string";
 	month jan = month("JAN");
+	// testing output of toUpper
+	cout << "before function\n" << testing << endl;
+	cout << toUpper(testing) << endl;
 	return (0);	
 }
 
+string toUpper(string str_org){
+	string	str_final;
+	str_final = str_org;
+	char test_char = 'x'; // testing 
+	
+	for (int i = 0 ; str_org[i] ; i++)
+	{
+		if (str_org[i] >= 'a' && str_org[i] <= 'z'){
+		str_final[i] = str_org[i] + ('A' - 'a');
+		} else {
+		str_final[i] = str_org[i];
+		}
+	}
+	return (str_final);
+};
+
 month::month(string month)
 {
-	month_index = 1;
-	cout << month << endl;
-	printf("the month index = %i\n",month_index);	
+	// finish thiss function
+	index = 1;
+	//cout << month << endl;
 }
