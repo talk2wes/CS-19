d864a7736a5e8903f41e9c2686ac12983ae283d4
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-16 13:40:31 -0700

    working on genStr function

--- assignments/words.cpp
+++ assignments/words.cpp
@@ -7,22 +7,25 @@ using namespace std;
 unsigned const int	NUMBER_OF_STRINGS = pow(10, 6);
 
 // Generates a random string within the ascii values specified
-string	genStr(char start, char end){
+string	genStr(){
 	string 		newString;
 	unsigned char	randChar;
-
+	
+	newString = ""; //DUMMY RETURN VALUE 
 	return (newString);
 }
 
 
 int	main(int argc, char** argv){
-	vector<string>	test;
-	int				totalChars;
-	double				average;	
+	vector<string>	allNumber;
+	int		totalChars;
+	double		average;	
 
+	allNumber.reserve(NUMBER_OF_STRINGS);
 	//generate strings and store them into vector allString
 	for (int i = 0; i < NUMBER_OF_STRINGS; i++){
-		
+		printf("Loop i = %i\n",i);
+		allNumber[i] = genStr();
 	}
 	//find sum of all string lengths
 	//divide total string lengths by the number of string to find the mean
