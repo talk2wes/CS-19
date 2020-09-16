9a4bf9294f7342bb19a0778a0c49978772cabfd2
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-16 13:57:44 -0700

    working on genStr function

--- assignments/words.cpp
+++ assignments/words.cpp
@@ -10,25 +10,34 @@ unsigned const int	NUMBER_OF_STRINGS = pow(10, 6);
 string	genStr(){
 	string 		newString;
 	unsigned char	randChar;
-	
-	newString = ""; //DUMMY RETURN VALUE 
+	unsigned char	stopChar;
+
+	stopChar = 'Z' + 1;
+	while (randChar != stopChar){
+		
+	}
+	newString = "l"; //DUMMY RETURN VALUE 
 	return (newString);
 }
 
 
 int	main(int argc, char** argv){
 	vector<string>	allNumber;
-	int		totalChars;
+	long int	totalChars;
 	double		average;	
 
+	totalChars = 0;
 	allNumber.reserve(NUMBER_OF_STRINGS);
 	//generate strings and store them into vector allString
 	for (int i = 0; i < NUMBER_OF_STRINGS; i++){
-		printf("Loop i = %i\n",i);
 		allNumber[i] = genStr();
+		//find sum of all string lengths
+		totalChars += (allNumber[i]).length();
+		printf("Loop i = %i length = %li\n",i,totalChars); // DEBUGGING
 	}
-	//find sum of all string lengths
 	//divide total string lengths by the number of string to find the mean
+	average = (double) totalChars / NUMBER_OF_STRINGS;
 	//format the output to one decimal place 
+	printf("Average = %.1f\n",average);
 	return(0);
 }
