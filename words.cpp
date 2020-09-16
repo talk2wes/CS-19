8849a2f7b63f273bf9d543e320363ea6fb88f986
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-15 22:25:05 -0700

    assignment 1 update

--- words.cpp
+++ words.cpp
@@ -1,8 +1,27 @@
 #include <string>
 #include <iostream>
+#include <cmath> 
 using namespace std;
 
+unsigned const int	NUMBER_OF_STRINGS = pow(10, 6);
+
+// Generates a random string within the ascii values specified
+string	genStr(char start, char end){
+	string 		newString;
+	unsigned char	randChar;
+
+	return (newString);
+}
+
+
 main(){
-	
+	vector<string>	allString(NUMBER_OF_STRINGS);
+	int		totalChars;
+	double		average;	
+
+	//generate strings and store them into vector allString
+	//find sum of all string lengths
+	//divide total string lengths by the number of string to find the mean
+	//format the output to one decimal place 
 	return(0);
 }
