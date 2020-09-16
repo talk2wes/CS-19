c37e183e967c5f1f227ef6659990b012cc4d547f
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-16 12:07:32 -0700

    just getting the program started,  added the vector library  and it compiles

---
+++ assignments/words.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include <cmath> 
+#include <vector>
+using namespace std;
+
+unsigned const int	NUMBER_OF_STRINGS = pow(10, 6);
+
+// Generates a random string within the ascii values specified
+string	genStr(char start, char end){
+	string 		newString;
+	unsigned char	randChar;
+
+	return (newString);
+}
+
+
+int	main(int argc, char** argv){
+	vector<string>	test;
+	int				totalChars;
+	double				average;	
+
+	//generate strings and store them into vector allString
+	for (int i = 0; i < NUMBER_OF_STRINGS; i++){
+		
+	}
+	//find sum of all string lengths
+	//divide total string lengths by the number of string to find the mean
+	//format the output to one decimal place 
+	return(0);
+}
