74378d3423ff3c363f486c732554d97159af1225
Wesley Johanson <talk2wes@gmail.com>
2020-11-18 10:02:19 -0800

    made a lot of progress. Working on the constructor for Dictionary class. trying to get it to open up the right filename

--- assignments/05/Cinco.cpp
+++ assignments/05/Cinco.cpp
@@ -1,17 +1,84 @@
 #include <iostream>
+#include <vector>
+#include <set>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
 
 
 class Dictionary{
-	
+	public:
+		Dictionary(char* filename);
+		bool				validWord(std::string word);
+		std::string 			getLegalSecretWord();
+	private:
+		bool			isLegalSecretWord(std::string word);
+		std::set<std::string> 		words;
+		std::vector<std::string> 	secrets;
+		const std::string DEFAULT_FILENAME = "cinco-words.txt";
 };
+
+Dictionary::Dictionary(char* filename){
+	srand(time(0));
+	std::fstream file;
+	
+
+	std::cout << "test 999\n";
+	//should us a try-catch statement incase the file doesn't open 
+	if (filename){
+		std::cout << "custom filename\n";
+		file.open(filename);
+	}else{
+		std::cout << "default filename\n";
+		file.open(DEFAULT_FILENAME);
+	}
+	
+	std::string temp = "";
+	while (file >> temp){
+		std::cout << temp << "\n";
+	}
+	file.close();
+
+}
 
 class Cinco{
-
+	public:
+		Cinco(std::string filename = "");
+		~Cinco();
+		void		play();
+	private:
+		int		countMatchingLetters(std::string);
+		int		countInPlaceLetters(std::string);
+		Dictionary*	dict;
+		int		numOfGuesses;
+		bool		cheated;
+		std::string	secret;
 };
 
+void	Cinco::play(){
+	
+}
 
+Cinco::Cinco(std::string filename){
+	dict = new Dictionary((char*) filename.c_str());
+	cheated = false;
+	//secret = dict.getLegalSecretWord();
+	//numOfGuesses = 0;
+}
+
+Cinco::~Cinco(){
+	delete[] dict;
+	dict = NULL;
+}
 
 int	main(int argc, char** argv){
 
-	std::cout << "hello world\n";
+	class Cinco* game;
+	if (argc > 1)
+		game = new Cinco(argv[1]);
+	else
+		game = new Cinco();
+	game->play();
+	
 }
