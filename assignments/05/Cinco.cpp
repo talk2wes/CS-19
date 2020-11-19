9d6f327e219b8de170a2f7f834151ad0455f2e32
Wesley Johanson <talk2wes@gmail.com>
2020-11-18 19:23:16 -0800

    almost done writing the code, working on the play function of cinco class. Haven't done any testing yet but it shouldn't be too bad since it's rewritten code from the java code

--- assignments/05/Cinco.cpp
+++ assignments/05/Cinco.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 
+//I need to make sure that the input is NOT CASE SENSITIVE
 
 class Dictionary{
 	public:
@@ -17,13 +18,14 @@ class Dictionary{
 		std::set<std::string> 		words;
 		std::vector<std::string> 	secrets;
 		const std::string DEFAULT_FILENAME = "cinco-words.txt";
+		const int 		WORD_LENGTH = 5; 
+		//length of all words in dictionary ^^ 
 };
 
 Dictionary::Dictionary(char* filename){
 	srand(time(0));
 	std::fstream file;
 	
-
 	std::cout << "test 999\n";
 	//should us a try-catch statement incase the file doesn't open 
 	if (strcmp(filename, "")){
@@ -36,11 +38,31 @@ Dictionary::Dictionary(char* filename){
 	
 	std::string temp = "";
 	while (file >> temp){
+		words.insert(temp);	
 		std::cout << temp << "\n";
+		if (this->isLegalSecretWord(temp))
+			secrets.push_back(temp);
 	}
 	file.close();
-
 }
+
+bool	Dictionary::validWord(std::string word){ return (words.count(word)); }
+
+bool	Dictionary::isLegalSecretWord(std::string word){
+	if (word.length() != WORD_LENGTH)
+		return false;
+	for (int i = 0; i < WORD_LENGTH; i++){
+		for (int j = 0; j < WORD_LENGTH; j++){
+			if (i != j && word[i] == word[j])
+				return false;
+		}
+	}
+	return true;
+}
+
+std::string	Dictionary::getLegalSecretWord(){ return (secrets[rand() % secrets.size()]); }
+
+// CINCO CLASS 
 
 class Cinco{
 	public:
@@ -54,23 +76,53 @@ class Cinco{
 		int		numOfGuesses;
 		bool		cheated;
 		std::string	secret;
+		const int	WORD_LENGTH = 5;
 };
 
 void	Cinco::play(){
+	//STOP 
 	
 }
 
 Cinco::Cinco(std::string filename){
 	dict = new Dictionary((char*) filename.c_str());
 	cheated = false;
-	//secret = dict.getLegalSecretWord();
-	//numOfGuesses = 0;
+	secret = dict->getLegalSecretWord();
+	numOfGuesses = 0;
 }
 
 Cinco::~Cinco(){
 	delete[] dict;
 	dict = NULL;
 }
+
+int	Cinco::countMatchingLetters(std::string guess){
+	int diffs = 0;
+	int lettersInAlphabet = 26;
+	bool alphabet[lettersInAlphabet];
+	
+	for (int i = 0; i < lettersInAlphabet; i++)
+		alphabet[i] = false;
+
+	if (secret.length() != WORD_LENGTH)
+		return -1;
+
+	for (int i = 0; i < lettersInAlphabet; i++)
+		for (int j = 0; j < lettersInAlphabet; j++)
+			if (secret[i] == guess[j])
+				alphabet[secret[i] - 'a'] = true;
+	for (int i = 0; i < lettersInAlphabet; i++)
+		diffs = alphabet[i] ? diffs + 1 : diffs;
+	return diffs;
+}
+
+int 	Cinco::countInPlaceLetters(std::string guess){
+	int total = 0;
+	for (int i = 0; i < WORD_LENGTH; i++)
+		total = (guess[i] == secret[i]) ? total + 1 : total;
+	return total;
+}
+
 
 int	main(int argc, char** argv){
 
