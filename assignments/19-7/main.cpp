ded8ddb1db1f0f253d0f770cecf7294f5d18f1ed
Wesley Johanson <talk2wes@gmail.com>
2020-12-17 22:16:55 -0800

    Added some comments, needs some polishing and its done

--- assignments/19-7/main.cpp
+++ assignments/19-7/main.cpp
@@ -1,6 +1,8 @@
 /* Wesley Johanson	Pengo: Wjohanso		talk2wes@gmail.com
  * Filename: main.c
- * Assignment 7: Six degrees of bacon */
+ * Assignment 7: Six degrees of bacon 
+ *
+ * working and tested!!!!*/
 
 #include "bacon.h"
 
@@ -9,32 +11,29 @@ using std::ifstream;
 using std::cout;
 using std::vector;
 
+//prototype 
 bool removeWord(vector<string>&, string);
 
+//returns true if words differ by one character
 bool	isAdjacent(string word1, string word2){
 	int diffs = 0;
 	if (word1.size() != word2.size()) return false;
 
-	for (int i = 0 ; i < word1.size(); i++){
+	for (int i = 0 ; i < word1.size(); i++)
 		if (word1[i] != word2[i]) diffs++;	
-		//cout << "diffs = " << diffs << std::endl;
-	}
-	//if (diffs == 1) cout << word1 << "\t" << word2 << std::endl;
 	return diffs == 1;
 }
 
+//finds the node that contains "bacon" in the last col of the solution space 
 WNodePtr findSolutionNode(vector<vector<WNodePtr>*>& solutionSpace, string endWord){
 	int lastColInd = solutionSpace.size() - 1;
-	//cout << "last col = " << lastColInd << "\n";
-	for (int i = 0; i < solutionSpace[lastColInd]->size(); i++){
-		//cout << solutionSpace[lastColInd]->at(i)->word << "\n";
-		if (endWord.compare(solutionSpace[lastColInd]->at(i)->word) == 0){
+	for (int i = 0; i < solutionSpace[lastColInd]->size(); i++)
+		if (endWord.compare(solutionSpace[lastColInd]->at(i)->word) == 0)
 			return (solutionSpace[lastColInd]->at(i));
-		}
-	}
 	return 0;
 }
 
+//recursively finds the first word and then prints path in reverse
 void 	printReverseSolution(WNodePtr bacon){
 	if (bacon->link == 0){
 		cout << bacon->word << "\n";
@@ -45,6 +44,7 @@ void 	printReverseSolution(WNodePtr bacon){
 	}
 }
 
+//prints the path, starting from bacon
 void	printSolution(WNodePtr bacon){
 	WNodePtr temp = bacon;
 	while (temp != 0){
@@ -52,14 +52,15 @@ void	printSolution(WNodePtr bacon){
 		temp = temp->link;
 	}
 }
+
+/*finds all adjacent words in the i'th vector, stores them in a new vector
+ *and then adds that vector to the solution space*/
 bool	findAdjacentWords(vector<vector<WNodePtr>*>& solutionSpace,
 						vector<string>* dictionary, 
 						string endWord){
 	bool solutionFound = false;
 	for (int i = 0; i < solutionSpace.size(); i++){
 		WNodePtrVec* newVector = new WNodePtrVec();
-		//find all words in dictionary adj to words in i'th column
-		//cout << "col = " << i << "\n";
 		for (int j = 0; j < solutionSpace[i]->size(); j++){
 			//find all words adj to word at [i,j]
 			for (string word : *dictionary){
@@ -80,8 +81,6 @@ bool	findAdjacentWords(vector<vector<WNodePtr>*>& solutionSpace,
 		//Add vector to solutionSpace if words were found
 		if (newVector->size() > 0){
 			solutionSpace.push_back(newVector);
-			//cout << "col = " << solutionSpace.size() << "\n";
-			//if (solutionFound){ cout << "solutionfound!!\n";}
 			if (solutionFound) return true;
 		}else //return if no adjacencies found in the last vector
 			return false;
@@ -89,24 +88,15 @@ bool	findAdjacentWords(vector<vector<WNodePtr>*>& solutionSpace,
 	return false;
 }
 
+//returns true if the word is found in the dictionary 
 bool	hasWord(vector<string> dictionary, string word){
 	for (int i = 0; i < dictionary.size(); i++)
 		if (word.compare(dictionary[i]) == 0)
 			return true;
 	return false;
 }
-/*
-bool	removeWord(vector<string>* dictionary, string word){
-	for (int i = 0; i < dictionary->size(); i++){
-		if (word.compare(dictionary->at(i)) == 0){
-			dictionary->erase(dictionary->begin() + i);
-			return true;
-		}
-	}
-	return false;
-}
-*/
 
+//removes the word from the dictionary 
 bool	removeWord(vector<string>& dictionary, string word){
 	for (int i = 0; i < dictionary.size(); i++){
 		if (word.compare(dictionary[i]) == 0){
@@ -117,12 +107,23 @@ bool	removeWord(vector<string>& dictionary, string word){
 	return false;
 }
 
+//checks if the user input is a valid word to start with
+bool	inputValid(string word, vector<string> dictionary){
+		if (word.size() != 5){
+			cout << "choose a 5 letter word\n";
+			return false;
+		}
+		if (hasWord(dictionary, word) == false){
+			cout << "word not known (aka not in dictionary)\n";
+			return false;
+		}
+		return true;
+}
 int	main(int argc, char** argv){
 	
 	//Determine if flag option is used 
 	string flag = "-r";	
 	int flagIndex = -1;
-	//cout << "preflag\n";
 	if (argc < 2 && argc > 3) return -1; //Error: undefined arguments
 	if (argc == 3){
 		if (flag.compare(argv[1]) == 0) flagIndex = 1;
@@ -130,66 +131,53 @@ int	main(int argc, char** argv){
 		if (flagIndex == -1) return -1; //Error: incorrect flag option 
 	}
 	int fileIndex = (flagIndex == 1) ? 2 : 1;
-	//cout << "command line arguments are valid\n";
 
 	//open the file
 	ifstream fin;
 	fin.open(argv[fileIndex]);
 	if (fin.fail()) return -1; //error opening file
-	//cout << "file was opened successfully\n";
 	
 	//read the file into the dictionary
 	vector<string>* dictionary = new vector<string>();
 	string temp;
 	while (fin >> temp)
 		dictionary->push_back(temp);
-	//test that I loaded the dictionary
-	/*
-	for (int i = 0; i < dictionary.size(); i++)
-		cout << dictionary[i] << std::endl;
-	*/
 	
 	//Program initial output
 	cout << "Six Degrees of Bacon. Solution by Wesley Johanson\n";
 	cout << "Wesley Johanson, talk2wes@gmail.com, pengo: wjohanso\n";
 	cout << "Your word?";
-	string userInput;
-	std::cin >> userInput;
-	//MAKE THIS LOWER CASE AND CHECK FOR NON_ALPHA CHARACTERS. Ask again if invalid
-	//if -r option is made. Make sure the userInput word is IN THE DICTIONARY
 
-	string startWord = userInput;
+	//Get the starting word from the user
+	string startWord;
+	std::cin >> startWord;
 	string endWord = "bacon";
-	//determine where to start and end
-	/*
-	string startWord = (flagIndex > -1) ? userInput : "bacon";
-	string endWord = (flagIndex > -1 ) ? "bacon" : userInput;
-	*/
-	//do I need to remove the startword from the dictionary? 
+	while (inputValid(startWord, *dictionary) == false){
+		cout << "Your word?";
+		std::cin >> startWord;
+	}
 
-	//Make the solutionspace and add the starting word
+	//Initialize the solutionspace and add the starting word
 	vector<vector<WNodePtr>*>* solutionSpace = new vector<WNodePtrVec*>();
 	vector<WNodePtr>* startVec = new vector<WNodePtr>();
 	WNode* startNode = new WNode(startWord);
 	startVec->push_back(startNode);
 	solutionSpace->push_back(startVec);
 
-	//remove the startword from the dictionary
+	//find all adjacencies, until bacon found or no solution
 	if (findAdjacentWords(*solutionSpace, dictionary, endWord)){
+		//Solution was found
 		WNodePtr bacon = findSolutionNode(*solutionSpace, endWord);
-		printSolution(bacon);
-		cout << "reverse SOlution! \n";
-		printReverseSolution(bacon);
+		if (flagIndex == -1)
+			printSolution(bacon);
+		else
+			printReverseSolution(bacon);
+	}else{ 
+		//No solution found
+		if (flagIndex > -1 )
+			cout << "no path from " << startWord << " to " << endWord << "\n";
+		else
+			cout << "no path from " << endWord << " to " << startWord << "\n";
 	}
-	//cout << "start: " << startWord << "\nend: " << endWord << std::endl;
-	//cout << "has startWord? : " << hasWord(*dictionary, startWord) << std::endl;
-	//cout << "remove startWord? : " << removeWord(*dictionary, startWord) << std::endl;
-
-	/*
-	for (int i = 0; i < dictionary->size(); i++){
-		if (isAdjacent(dictionary->at(i), startWord) == true)
-			cout << "start: " << startWord << "\tadj: " << dictionary->at(i) << "\n";
-	*/
-	
 	return 1;
 }
